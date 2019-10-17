dd788aeb83571f92908a6f1f8d38126d5c9b7ceb
Adam Paxton <adampaxton9973@gmail.com>
2019-10-17 17:39:14 +0100

    Renamed module and added electricity usage feature

--- prisoner_games.py
+++ prisoner_games.py
@@ -1,14 +1,16 @@
 import random
-#question: can we import random within the definition??
 
-def play_prisoner_lightswitch_game(number_of_prisoners):
+def play_lightswitch_game(number_of_prisoners):
     N = number_of_prisoners
     counting_list = [False]*N
     light = False
     days = 0
+    electricity_bill = 0
     prisoner_count = 0
     while prisoner_count < N-1:
         days = days + 1
+        if light == True:
+            electricity_bill = electricity_bill + 1
         m = random.randint(0,N-1)
         if m > 0 and light == False and counting_list[m] == False:
             light = True
@@ -16,4 +18,4 @@ def play_prisoner_lightswitch_game(number_of_prisoners):
         elif m == 0 and light == True:
             light = False
             prisoner_count = prisoner_count + 1
-    return "the prisoners were released after %s days" % days
+    return "the prisoners were released after %s days" % days, "the light was on for %s days" % electricity_bill
